b5238cc8f9d2faa3fb1ac02eb2342b5f212f3fdc
SaachiKaup <saachi.kaup@gmail.com>
2021-09-21 01:07:02 +0530

    Commiting version of p1 again wih same name

--- p1.py
+++ p1.py
@@ -15,22 +15,22 @@ data = data.dropna()
 data["Positive"] = [sentiments.polarity_scores(i)["pos"] for i in data["Text"]]
 data["Negative"] = [sentiments.polarity_scores(i)["neg"] for i in data["Text"]]
 data["Neutral"] = [sentiments.polarity_scores(i)["neu"] for i in data["Text"]]
-print(data.head())
+#print(data.head())
 
 x = sum(data["Positive"])
 y = sum(data["Negative"])
 z = sum(data["Neutral"])
 
-def sentiment_score(a, b, c):
-    if (a>b) and (a>c):
-        print("Positive 😊 ")
-    elif (b>a) and (b>c):
-        print("Negative 😠 ")
-    else:
-        print("Neutral 🙂 ")
+#def sentiment_score(a, b, c):
+#    if (a>b) and (a>c):
+#        print("Positive 😊 ")
+#    elif (b>a) and (b>c):
+#        print("Negative 😠 ")
+#    else:
+#        print("Neutral 🙂 ")
 
-sentiment_score(x, y, z)
+#sentiment_score(x, y, z)
 
-print("Positive: ", x)
-print("Negative: ", y)
-print("Neutral: ", z)
+#print("Positive: ", x)
+#print("Negative: ", y)
+#print("Neutral: ", z)
