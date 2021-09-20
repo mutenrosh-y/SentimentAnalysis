ffe2d3640694aafa95ebaf63467057c4edfce417
SaachiKaup <saachi.kaup@gmail.com>
2021-09-21 00:49:36 +0530

    Removed print statements to import variables

---
+++ p1 (1).py
@@ -0,0 +1,36 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+sentiments = SentimentIntensityAnalyzer()
+
+data = pd.read_csv("Reviews2.csv")
+
+ratings = data["Score"].value_counts()
+numbers = ratings.index
+quantity = ratings.values
+
+data = data.dropna()
+
+data["Positive"] = [sentiments.polarity_scores(i)["pos"] for i in data["Text"]]
+data["Negative"] = [sentiments.polarity_scores(i)["neg"] for i in data["Text"]]
+data["Neutral"] = [sentiments.polarity_scores(i)["neu"] for i in data["Text"]]
+#print(data.head())
+
+x = sum(data["Positive"])
+y = sum(data["Negative"])
+z = sum(data["Neutral"])
+
+#def sentiment_score(a, b, c):
+#    if (a>b) and (a>c):
+#        print("Positive 😊 ")
+#    elif (b>a) and (b>c):
+#        print("Negative 😠 ")
+#    else:
+#        print("Neutral 🙂 ")
+
+#sentiment_score(x, y, z)
+
+#print("Positive: ", x)
+#print("Negative: ", y)
+#print("Neutral: ", z)
