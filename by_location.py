7df1cddd33026b9643e5b1b21a16b19cac382297
SaachiKaup <saachi.kaup@gmail.com>
2021-09-21 01:04:03 +0530

    geoplotlib to plot by location. Needs changing.

---
+++ by_location.py
@@ -0,0 +1,8 @@
+from p1 import *
+import pyglet
+import geoplotlib
+from geoplotlib.utils import read_csv
+
+df = read_csv("Reviews.csv")  #replace path with your file path
+geoplotlib.dot(df)
+geoplotlib.show()
